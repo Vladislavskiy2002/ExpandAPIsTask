3b9be698d5c91356133d5b31bba8a2f4361be27c
vladi <vladislavskiyproduction@gmail.com>
2023-12-24 15:44:39 +0200

    added AuthRequest

---
+++ src/main/java/com/vladislavskiy/ExpandAPIsTask/AuthRequest.java
@@ -0,0 +1,14 @@
+package com.vladislavskiy.ExpandAPIsTask;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+    private String username;
+    private String password;
+
+}
