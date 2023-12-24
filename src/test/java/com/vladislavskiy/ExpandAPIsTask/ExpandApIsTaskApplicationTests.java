5293a6872081e65a851878c4632748b31c64055c
vladi <vladislavskiyproduction@gmail.com>
2023-12-24 12:07:21 +0200

    Initial commit

---
+++ src/test/java/com/vladislavskiy/ExpandAPIsTask/ExpandApIsTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladislavskiy.ExpandAPIsTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpandApIsTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
