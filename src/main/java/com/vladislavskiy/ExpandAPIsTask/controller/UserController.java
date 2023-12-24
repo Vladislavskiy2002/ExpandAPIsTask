d4b376d17d7a3ea5165f2090dff5b10041965215
vladi <vladislavskiyproduction@gmail.com>
2023-12-24 15:47:31 +0200

    added UserController

---
+++ src/main/java/com/vladislavskiy/ExpandAPIsTask/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.vladislavskiy.ExpandAPIsTask.controller;
+import com.vladislavskiy.ExpandAPIsTask.UserInfo;
+import com.vladislavskiy.ExpandAPIsTask.service.UserInfoService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+    private final UserInfoService service;
+
+    public UserController(@Qualifier("userInfoService") UserInfoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/isUserExist/{username}")
+    public String isUserWithNameExist(@PathVariable String username) {
+        return service.checkUserOnExistByUsername(username).toString();
+    }
+
+    @PostMapping("/addNewUser")
+    public String addNewUser(@RequestBody UserInfo userInfo) {
+        return service.addUser(userInfo);
+    }
+
+}
