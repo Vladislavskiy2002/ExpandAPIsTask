071e8c8fc31615b20d41e8900c47017ce73f33ef
vladi <vladislavskiyproduction@gmail.com>
2023-12-24 15:47:23 +0200

    added UserInfoService

---
+++ src/main/java/com/vladislavskiy/ExpandAPIsTask/service/UserInfoService.java
@@ -0,0 +1,43 @@
+package com.vladislavskiy.ExpandAPIsTask.service;
+
+import com.vladislavskiy.ExpandAPIsTask.UserInfo;
+import com.vladislavskiy.ExpandAPIsTask.UserInfoDetails;
+import com.vladislavskiy.ExpandAPIsTask.repository.UserInfoRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Slf4j
+public class UserInfoService implements UserDetailsService {
+    @Autowired
+    private UserInfoRepository repository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<UserInfo> userDetail = repository.findByName(username);
+
+        // Converting userDetail to UserDetails
+        return userDetail.map(UserInfoDetails::new).orElseThrow(() -> new UsernameNotFoundException("User not found " + username));
+    }
+
+    public String addUser(UserInfo userInfo) {
+        userInfo.setPassword(passwordEncoder.encode(userInfo.getPassword()));
+        repository.save(userInfo);
+        return "User Added Successfully";
+    }
+
+    public Boolean checkUserOnExistByUsername(String username) {
+        Optional<UserInfo> userDetail = repository.findByName(username);
+        return userDetail.isPresent();
+    }
+}
