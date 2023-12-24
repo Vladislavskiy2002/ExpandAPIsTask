c8db0ef60c84e6799333e70c2d6d79a6e481e4f5
vladi <vladislavskiyproduction@gmail.com>
2023-12-24 15:50:54 +0200

    added AuthController

---
+++ src/main/java/com/vladislavskiy/ExpandAPIsTask/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.vladislavskiy.ExpandAPIsTask.controller;
+
+import com.vladislavskiy.ExpandAPIsTask.AuthRequest;
+import com.vladislavskiy.ExpandAPIsTask.service.JwtService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/token")
+public class AuthController {
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthController(JwtService jwtService, AuthenticationManager authenticationManager) {
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping("/generateToken")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getUsername());
+        } else {
+            throw new UsernameNotFoundException("invalid user request !");
+        }
+    }
+}
